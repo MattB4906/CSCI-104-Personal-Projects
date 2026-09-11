1f0d76169bfcc4faea4b96c9a91a754eb5af0931
Matteo Benvenuti <matteo.benvenuti3310@icloud.com>
2026-09-10 21:33:44 -0700

    Changes made

---
+++ Trojan Help Desk Priority Queue/main.cpp
@@ -0,0 +1,116 @@
+#include "priorityqueue.h"
+#include "ticket.h"
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    PriorityQueue p{};
+    
+    char input = 'A';
+    int arrivalCounter = 0;
+    
+    
+    
+    while(input != 'Q') {
+        cout << "Press:" << endl;
+        cout << "A: add a ticket" << endl;
+        cout << "N: show the next ticket" << endl;
+        cout << "P: process/remove the next ticket" << endl;
+        cout << "L: list all tickets" << endl;
+        cout << "Q: quit" << endl;
+
+        if(!(cin >> input)) {
+            break;
+        }
+
+        if(input == 'A') {
+            int id;
+            
+            cout << "Id: ";
+            cin >> id;
+            
+            if(cin.fail()) {
+                cout << "Incorrect input!" << endl;
+        
+                cin.clear();
+                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+                continue;
+            }
+
+            cout << endl;
+
+
+            int priority;
+            cout << "Priority (between 1 and 5): ";
+            cin >> priority;
+
+            if(cin.fail()) {
+                cout << "Incorrect input!" << endl;
+                
+                cin.clear();
+                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                
+                continue;
+            }
+            
+            if(priority < 1 || priority > 5) {
+                cout << "Priority must be between 1 and 5!" << endl;
+                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+                continue;
+            }
+
+            cout << endl;
+
+            string description;
+            cout << "Description: ";
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            getline(cin, description);
+
+            if(cin.fail()) {
+                cout << "Incorrect input!" << endl;
+        
+                cin.clear();
+                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+                continue;
+            }
+
+            cout << endl;
+
+            Ticket t(id, priority, description, arrivalCounter);
+            p.add(t);
+            arrivalCounter++;
+
+        }
+
+        if(input == 'N') {
+            if(p.isEmpty()) {
+                cout << "Queue is empty" << endl;
+
+                continue;
+            }
+
+            cout << "Next: " << p.peek() << endl;
+        }
+
+        if(input == 'P') {
+            if(p.isEmpty()) {
+                cout << "Queue is empty" << endl;
+
+                continue;
+            }
+
+            cout << "Processing: " << p.remove() << endl;
+        }
+
+        if(input == 'L') {
+            p.print();
+        }
+    }
+
+    return 0;
+}
