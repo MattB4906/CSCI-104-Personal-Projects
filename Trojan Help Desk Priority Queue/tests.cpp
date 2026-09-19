88f4841ef7b4d815ac87a5b65554235ead468f6f
Matteo Benvenuti <matteo.benvenuti3310@icloud.com>
2026-09-18 22:44:50 -0700

    tests.cpp

--- Trojan Help Desk Priority Queue/tests.cpp
+++ Trojan Help Desk Priority Queue/tests.cpp
@@ -70,6 +70,55 @@ int main() {
     assert(q.remove() == t3);
     assert(q.getSize() == 0);
     assert(q.isEmpty());
+
+    PriorityQueue q1;
+    Ticket t_1(101, 1, "Test", 0);
+    Ticket t_2(102, 2, "Test", 1);
+    Ticket t_3(103, 3, "Test", 2);
+
+    q1.add(t_1);
+    q1.add(t_2);
+    q1.add(t_3);
+
+    PriorityQueue q2 = q1;
+
+    assert(q1.getSize() == 3 && q2.getSize() == 3);
+    assert(q1.peek() == t_1 && q2.peek() == t_1);
+
+    assert(q2.remove() == t_1);
+    assert(q2.peek() == t_2 && q2.getSize() == 2);
+    assert(q1.peek() == t_1 && q1.getSize() == 3);
     
+    Ticket t_4(104, 0, "Test", 3);
+    q1.add(t_4);
+    
+    assert(q1.peek() == t_4 && q1.getSize() == 4);
+    assert(q2.peek() == t_2 && q2.getSize() == 2);
+
+    assert(q1.remove() == t_4);
+    assert(q1.getSize() == 3);
+
+    assert(q1.peek() == t_1);
+    assert(q.remove() == t_1);
+    assert(q.getSize() == 2);
+
+    assert(q1.peek() == t_2);
+    assert(q1.remove() == t_2);
+    assert(q1.getSize() == 1);
+
+    assert(q1.peek() == t_3);
+    assert(q1.remove() == t_3);
+    assert(q1.getSize() == 0);
+    assert(q1.isEmpty());
+
+    assert(q2.peek() == t_2);
+    assert(q2.remove() == t_2);
+    assert(q.getSize() == 1);
+
+    assert(q2.peek() == t_3);
+    assert(q2.remove() == t_3);
+    assert(q2.getSize() == 0);
+    assert(q2.isEmpty());    
+
     return 0;
 }
